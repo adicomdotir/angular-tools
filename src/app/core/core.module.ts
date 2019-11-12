60b5f7c3d2e3f8be771a9758166ceba37a5e268b
adicomdotir <adicom.ir@gmail.com>
2019-11-12 21:33:40 +0330

    Add folder structure

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    imports: [RouterModule, CommonModule],
+    exports: [],
+    providers: [],
+    declarations: []
+
+})
+export class CoreModule { }
